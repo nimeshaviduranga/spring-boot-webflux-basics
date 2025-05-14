189f16e73e56b247f97504df86305b971216e202
nimeshaviduranga <49368109+nimeshaviduranga@users.noreply.github.com>
2025-05-14 20:34:16 +0530

    Initial configs

---
+++ src/main/java/com/example/springbootwebfluxbasics/SpringBootWebfluxBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebfluxbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebfluxBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebfluxBasicsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootwebfluxbasics/SpringBootWebfluxBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebfluxbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxBasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
